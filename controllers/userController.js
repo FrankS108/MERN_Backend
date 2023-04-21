5b1d4c458784e5fd4c0eeb5c19bfb6a998575bd4
Frank <francisco.javier.huerta.silva@uabc.edu.mx>
2023-04-20 18:56:39 -0700

    Actualizando eliminación de usuarios

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,13 +22,13 @@ const getUsers = async (req, res) => {
 
 const deleteUser = async(req, res) => {
     try{
-        await User.findOneAndDelete(req.body._id);
+        //await User.findOneAndDelete(req.body.id);
+        await User.findByIdAndDelete(req.body.id);
         res.json({msg: "Eliminado Correctamente"});
     }catch(error){
         console.log(error);
     }
 }
-
 
 export {
     register,
